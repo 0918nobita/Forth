9312789591c40ae55d72e6566bec0ec1bdf60c48
0918nobita <nobita.0918@gmail.com>
2019-02-14 03:25:08 +0900

    :sparkles: Starting FORTH の第1章の問題の解答を追加した

--- chap1.fs
+++ chap1.fs
@@ -6,3 +6,22 @@
 : blip  margin star ;
 : bar  margin 5 stars ;
 : f  bar blip bar blip blip cr ;
+
+( Problems )
+
+( 1 )
+: gift ." BOOKENDS" ;
+: giver ." STEPHANIE" ;
+: thanks cr ." DEAR " giver ." ," cr 4 spaces ." THANKS FOR THE " gift ." ." ;
+
+( 2 )
+: ten.less -10 + ;
+
+( 
+  3: After entering the words in Prob. 1,
+  enter a new definition for GIVER to print someone else's name,
+  then execute THANKS again. Can you explain why THANKS still prints
+  out the first giver's name ?
+
+  辞書は、ワードの再定義が行われても既存のワードの参照が保持される仕様になっているため 
+)
