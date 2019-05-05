aece44aac7681ea082bd08dc85fbfc0a8f3e00fd
0918nobita <nobita.0918@gmail.com>
2019-05-05 12:22:14 +0900

    :construction: WebAssembly.Module クラスのアンビエント宣言を追加した

--- wasm/src/index.ts
+++ wasm/src/index.ts
@@ -1,4 +1,28 @@
 namespace WebAssembly {
+  export declare class Module {
+    /**
+     * Returns a copy of the contents of all custom sections in the given module with the given string name.
+     * @param module The `WebAssembly.Module` object whose custom sections are being considered.
+     * @param sectionName The string name of the desired custom section.
+     */
+    static customSections(module: Module, sectionName: string): ArrayBuffer;
+
+    /**
+     * Returns an array containing descriptions of all the declared exports of the given `Module`.
+     * @param module A `WebAssembly.Module` object.
+     * @returns An array containing objects representing the exported functions of the given module.
+     */
+    static exports(module: Module): object;
+
+    /**
+     * Returns an array containing descriptions of all the declared imports of the given `Module`.
+     * @param module A `WebAssembly.Module` object.
+     * @returns An array containing objects representing the imported functions of the given module.
+     */
+    static imports(module: Module): object;
+  
+    toString(): 'WebAssembly.Module';
+  }
 }
 
 const memory = new WebAssembly.Memory({ initial: 1 });
