9f79c2e74e96d5a24bdc81182cecefac381d3aad
0918nobita <nobita.0918@gmail.com>
2019-05-05 13:28:22 +0900

    :construction: WebAssembly.Compile/Link/RuntimeError クラスのアンビエント宣言を追加した

--- wasm/src/index.ts
+++ wasm/src/index.ts
@@ -23,6 +23,98 @@ namespace WebAssembly {
   
     toString(): 'WebAssembly.Module';
   }
+
+  export declare class Global {
+    value: any;
+    constructor(descriptor: { value: 'i32' | 'i64' | 'f32' | 'f64', mutable?: boolean }, value: any);
+    toString(): 'WebAssembly.Global';
+    valueOf(): any;
+  }
+
+  export declare class Instance {
+    /**
+     * Returns an object containing as its members all the functions exported from the WebAssembly module instance,
+     * to allow them to be accessed and used by JavaScript.
+     */
+    readonly exports: { [K: string]: Function };
+
+    /**
+     * @param module The `WebAssembly.Module` object to be instantiated.
+     * @param importObject An object containing the values to be imported into newly-created `Instance`,
+     * such as functions or `WebAssembly.Memory` objects.
+     * There must be one matching property for each declared import of `module` or else a WebAssembly.LinkError is thrown.
+     */
+    constructor(module: Module, importObject?: object);
+
+    /**
+     * Increases the size of the memory instance by a specified number of WebAssembly pages.
+     * @param pages The number of WebAssembly pages you want to grow the memory by (each one is 64KiB in size) .
+     * @returns The previous size of the memory.
+     */
+    grow(pages: number): number;
+  }
+
+  /**
+   * A memory created by JavaScript or in WebAssembly code will be accessible and mutable
+   * from both JavaScript and WebAssembly.
+   */
+  export declare class Memory {
+    /**
+     * An accessor property that returns the buffer contained in the memory.
+     */
+    buffer: ArrayBuffer;
+
+    /**
+     * Creates a new `Memory` object which is a resizable ArrayBuffer
+     * that holds the raw bytes of memory accessed by a WebAssembly `Instance`.
+     */
+    constructor(memoryDescriptor: { initial: number; maximum?: number; });
+  }
+
+  export declare class Table {
+    /** the length of the table */
+    length: number;
+
+    constructor(tableDescriptor: { element: string; initial: number; maximum?: number; });
+
+    /**
+     * Retrives a function reference stored at a given index.
+     * @param index The index of the function reference you want to retrive.
+     * @returns a function reference — this is exported WebAssembly function,
+     * a JavaScript wrapper for an underlying wasm function.
+     */
+    get(index: number): Function;
+
+    /**
+     * Increases the size of the Table instance by a specified number of elements.
+     * @param elements The number of elements you want to grow the table by.
+     * @returns The previous length of the table.
+     */
+    grow(elements: number): number;
+
+    /**
+     * Mutates a reference stored at a given index to a difference value.
+     * @param index The index of the function reference you want to mutate.
+     * @param value The value you want to mutate the reference to. This must be an exported WebAssembly function,
+     * a JavaScript wrapper for underlying wasm function.
+     */
+    set(index: number, value: Function): void;
+  }
+
+  /**
+   * Indicates an error during WebAseembly decoding or validation.
+   */
+  export declare class CompileError extends Error {}
+
+  /**
+   * Indicates an error during module instantiation.
+   */
+  export declare class LinkError extends Error {}
+
+  /**
+   * The type is thrown whenever WebAssembly specifies a trap.
+   */
+  export declare class RuntimeError extends Error {}
 }
 
 const memory = new WebAssembly.Memory({ initial: 1 });
