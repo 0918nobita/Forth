87ec77e65e85b258ccda12ad2b9147d8fde883db
0918nobita <nobita.0918@gmail.com>
2019-06-10 11:22:38 +0900

    :sparkles: 高階関数 2times を実装した

---
+++ high-order.fs
@@ -0,0 +1,20 @@
+: 2times  ( xt "<spaces>name" -- )
+  CREATE ,
+  DOES> @ SWAP OVER EXECUTE SWAP EXECUTE
+;
+
+: plus3  ( n -- n )  3 + ;
+
+' plus3 2times plus6 ( define plus6 )
+
+' plus6 2times plus12 ( define plus12 )
+
+3 plus12 . ( => 15 )
+
+: mul2  ( n -- n )  2 * ;
+
+' mul2 2times mul4 ( define mul4 )
+
+' mul4 2times mul16 ( define mul16 )
+
+2 mul16 . ( => 32 )
